3ba118e6c0c9fdc33f88f52e4e0c10368685b955
Stephan Kochauf <kc@htl-kaindorf.at>
2022-03-10 13:11:27 +0100

    Initial commit

---
+++ src/SearchArray.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.Random;
+
+
+// DON'T TOUCH THIS CLASS
+// Diese Klasse nicht ändern
+
+public class SearchArray {
+
+    private int[] array;
+    private int getCounter;
+    private int load;
+    private final int maxLength;
+
+    public SearchArray(int load, int maxLength){
+        getCounter = 0;
+        this.load = load;
+        this.maxLength = maxLength;
+    }
+
+    public void resetGetCounter(){
+        getCounter = 0;
+    }
+
+    public int getGetCounter() {
+        return getCounter;
+    }
+
+    public int length(){
+        return array.length;
+    }
+
+    public int get(int index){
+        // Making access a bit slower
+        for (int i = 0; i < load; i++)
+            Thread.yield();
+
+        getCounter++;
+        return array[index];
+    }
+
+    public void load(){
+        System.out.println("Loading started, may take some time . . .");
+        array  = new Random().ints((int) (maxLength * 1.2), 1_000_000 ,2_000_000_000)
+                .distinct().sorted().limit(maxLength).toArray();
+
+        System.out.println("Length of array: " + array.length);
+
+        array[0] = 314699018;
+        array[1] = 816387057;
+        array[2] = 1317297914;
+        array[3] = 1651147229;
+
+        Arrays.sort(array);
+    }
+}
---
+++ src/SearchDemo01SequentialSearch.java
@@ -0,0 +1,57 @@
+public class SearchDemo01SequentialSearch {
+
+    // Don't change this method - Diese Methode nicht ändern
+    public static void main(String[] args) {
+
+        System.out.println("Starting SearchDemo1");
+
+        SearchArray searchArray = new SearchArray(2, 10_000_000);
+        searchArray.load();
+        System.out.println("SearchDemo1: loading entries finished");
+
+        // count of entries ...
+        System.out.println("searchArray.length() = " + searchArray.length());
+        // get one element ...
+        System.out.println("searchArray.get(15) = " + searchArray.get(15));
+
+        System.out.println("searching ...");
+
+        // LOOKING for 314699018
+        searchFor(searchArray, 314699018);
+
+        searchFor(searchArray, 314699018);
+
+        // LOOKING for 816387057
+        searchFor(searchArray, 816387057);
+
+        // LOOKING for 1317297914
+        searchFor(searchArray, 1317297914);
+
+        // LOOKING for 1651147229
+        searchFor(searchArray, 1651147229);
+
+        // LOOKING for 1651147228
+        searchFor(searchArray, 1651147228);
+    }
+
+    public static int search(SearchArray array, int value, int left, int right){
+
+        // left = 0
+        // right = 9.999.999
+        // Your Code - Sequentielle Suche
+
+        return -1;
+    }
+
+    // Don't change this method - Diese Methode nicht ändern
+    private static void searchFor(SearchArray array, int value ){
+        System.out.print("Looking for " + value + " ");
+        array.resetGetCounter();
+        long startTime = System.currentTimeMillis();
+        int index = search(array, value,0 , array.length() - 1);
+        long stopTime = System.currentTimeMillis();
+
+        System.out.println("at position " + index + ", took " + array.getGetCounter() + " calls, and took " + (stopTime - startTime) + "ms");
+    }
+
+}
